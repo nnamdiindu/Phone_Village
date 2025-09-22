6f421c55d752e5a6814b9a131a737d6e7c7edac1
Lucky  Odi <luckyodi310@gmail.com>
2025-09-22 18:34:15 +0100

    Finish frontend code for login and sign up page

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ app.secret_key = os.environ.get("SECRET_KEY")
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
