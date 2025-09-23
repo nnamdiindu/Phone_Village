3ab7f9052df7211cbe311250761ef9f34dd534e6
Lucky  Odi <luckyodi310@gmail.com>
2025-09-23 11:07:33 +0100

    First profile page commit

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def login():
 def register():
     return render_template("register.html")
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
